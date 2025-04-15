2d9cd3dccd003ad7196a4647dbc4d28d390b153c
Cláudio Raposo <cfraposo@ciandt.com>
2025-04-15 20:15:10 -0300

    Update ProcessPendingImages.ts

--- application/ProcessPendingImages.ts
+++ application/ProcessPendingImages.ts
@@ -8,16 +8,21 @@ export class ProcessPendingImages {
         private readonly ocrService: IOCRService 
     ) {}
     async execute(): Promise<void> {
+        let error = [];
         const images = await this.imageRepository.getAndMarkPendingImages();
-
+        
         for (const image of images) {
             try {
                 const text = await this.ocrService.extractText(image.url);
                 const isPrescription = this.isPrescription(text);
                 await this.imageRepository.saveOcrResult(image.id, isPrescription);
             } catch (error) {
+                error.push(error);
                 await this.imageRepository.markAsError(image.id);
             }
+        }
+        if (error.length > 0) {
+            throw new Error(`Errors processing images: ${error.join(", ")}`);
         }
     }
     private isPrescription(text: string): boolean {
